68e55e728938232555ddaa04509f615d786a6fc8
1BMariaJoseRamirez <113257141+1BMariaJoseRamirez@users.noreply.github.com>
2022-09-11 11:46:02 -0600

    Add files via upload

---
+++ Periodo1Clase7.java
@@ -0,0 +1,17 @@
+package periodo1clase7;
+
+/**
+ *
+ * @author MariaJoseR
+ */
+public class Periodo1Clase7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
